e4aa1c7f5fa3bfbed6c901ef09154fcbb1fe9fe0
Angiras, Ashutosh (ICIS-KAR) <ashutosh.angiras@icis.com>
2026-07-11 00:04:27 +0200

    chore(terraform): add versions.tf with required_version (SUP-001)

---
+++ versions.tf
@@ -0,0 +1,6 @@
+terraform {
+  # Minimum Terraform version required by all modules in this repository.
+  # Use ~> to allow patch-level updates but pin the minor version.
+  # SUP-001 (platform-compliance) requires a pinned required_version constraint.
+  required_version = "~> 1.9"
+}
